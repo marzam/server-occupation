e4bea32e55828783951907721f871d5b92b6331d
root <root@qbert.dcc.ufrrj.br>
2019-11-11 16:02:31 -0300

    2019-11-11 release #1

--- uploadxml.php
+++ uploadxml.php
@@ -1,10 +1,4 @@
 <?php
-function myLog($msg){
-  $fileName = getRealIpAddr() . ".error.txt";
-  $handle = fopen($fileName, "w");
-  fwrite($handle,  $msg . "\n");
-  fclose($handle);
-}
 function getRealIpAddr(){
     if (!empty($_SERVER['HTTP_CLIENT_IP'])){
       $ip=$_SERVER['HTTP_CLIENT_IP'];
@@ -16,57 +10,68 @@ function getRealIpAddr(){
 
     return $ip;
 }
+function execute_sql($mysql){
+  $servername = "localhost";
+  $username   = "localuser";
+  $password   = "localuser";
+  $conn = new mysqli($servername, $username, $password);
+  if ($conn->connect_error) {
+     myLog("Error loading character set utf8:" . $conn->error);
+     return;
+  }
+  if (!$conn->set_charset("utf8")) {
+      myLog("Error loading character set utf8:" . $conn->error);
+      return;
+  }
 
-function execute_sql($conn, $mysql){
-	$result = $conn->query($mysql);
-	if ($conn->connect_error) {
-		myLog("Error quering:" . $conn->error);
-	}
+  $sql = "USE dbNetwork;";
+  $conn->query($sql);
+  if ($conn->connect_error) {
+     myLog("Error querinng:" . $conn->error);
+     return;
+  }
 
-	return $result;
+  $result = $conn->query($mysql);
+
+  $conn->close();
+  return $result;
 }
 
-$servername = "localhost";
-$username   = "localuser";
-$password   = "localuser";
-$conn = new mysqli($servername, $username, $password);
-if ($conn->connect_error) {
-	myLog("Error loading character set utf8:" . $conn->error);
-	return;
-}
-if (!$conn->set_charset("utf8")) {
-	myLog("Error loading character set utf8:" . $conn->error);
-	return;
-}
 
-$sql = "USE dbNetwork;";
-$conn->query($sql);
-if ($conn->connect_error) {
-	myLog("Error querinng:" . $conn->error);
-	return;
-}
+//  echo "Hello\n";
 
-$dataPOST = trim(file_get_contents('php://input'));
-$xmlData = simplexml_load_string($dataPOST);
+    $dataPOST = trim(file_get_contents('php://input'));
+    $xmlData = simplexml_load_string($dataPOST);
+
+    $fileName = "log/" . getRealIpAddr() . "." . date("Y_m_d-H_i_s", time()) . ".xml";
+
+    $handle = fopen($fileName, "w");
+    fwrite($handle,  $xmlData->asXML());
+    fclose($handle);
+
+    $records = $xmlData->children();
+    for ($i = 0; $i < $xmlData->count(); $i++){
+      $timestamp = $records[$i]->timestamp;
+      $device_id = $records[$i]->device_id;
+      $mac = strtoupper($records[$i]->mac);
+      $signal = $records[$i]->signal;
+      $result = execute_sql("SELECT ID FROM tbDeviceAddr WHERE addr='". $mac . "';");
+
+      if($field = $result->fetch_assoc()){
+        $id = $field['ID'];
+      }else {
+        execute_sql("INSERT INTO tbDeviceAddr (addr, type) VALUES ('". $mac . "', 'W');");
+        $result = execute_sql("SELECT ID FROM tbDeviceAddr WHERE addr='". $mac . "';");
+        $field = $result->fetch_assoc();
+        $id = $field['ID'];
+
+      }//end-if($id = $result->fetch_assoc()){
+      execute_sql("INSERT INTO tbNetworkRecord (date_time, device_id, device_signal, id_addr) VALUES ('" . $timestamp . "', '" . $device_id . "', '" . $signal . "', '" . $id . "');");
+
+      echo 'ID' . $id . PHP_EOL;
+    }//end-for ($i = 0; $i < $xmlData->count(); $i++){
+
+  echo 'END' . PHP_EOL;
 
 
-$records = $xmlData->children();
-$stringsql = "INSERT INTO tbNetworkRecord_v2 (date_time, device_id, device_signal, mac, pack_type) VALUES";
-
-for ($i = 0; $i < $xmlData->count(); $i++){
-	$timestamp = $records[$i]->timestamp;
-	$device_id = strtoupper($records[$i]->device_id);
-	$pack_type = $records[$i]->pack_type;
-	$mac = strtoupper($records[$i]->mac);
-	$signal = $records[$i]->signal;
-
-	$stringsql .= " ('" . $timestamp . "', '" . $device_id . "', '" . $signal . "', '" . $mac . "', '". $pack_type . "'),";
-
-}//end-for ($i = 0; $i < $xmlData->count(); $i++){
-$stringsql = substr( $stringsql , 0, -1) ;
-//echo $stringsql . PHP_EOL;
-var_dump(execute_sql($stringsql));
-//$conn->multi_query($stringsql);
-$conn->close();
-echo 'OK' . PHP_EOL;
 ?>
